01f3feeec6d23bb517807c3d3aaf8c286c15f831
Ashutosh Singh <ashutoshs.ug21.cs@nitp.ac.in>
2023-07-10 23:44:37 +0530

    Initial commit

---
+++ components/Carousely.js
@@ -0,0 +1,37 @@
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import styles from '../styles/Carousely.module.css';
+
+export default function Carousely() {
+  return (
+    <div className={styles['carousel-container']}>
+      <Carousel showThumbs={false}>
+        <div className={`${styles.slide} grid grid-cols-2`}>
+          <div className={`content w-1/2 p-4 ${styles['left-content']}`}>
+            <h2 className="text-2xl font-bold">Left Content</h2>
+            <p className="text-lg">Some text or other components</p>
+          </div>
+          <div className={`image w-1/2 flex items-center justify-center ${styles['right-content']}`}>
+            <img
+              src="http://www.cicba.in/cicba2023/images/slide1.jpg"
+              alt="image1"
+              className={styles['carousel-image']}
+            />
+          </div>
+        </div>
+        <div className={styles.slide}>
+          <div className={styles['left-half']}>
+            <h2>Left Content</h2>
+            <p>Some text or other components</p>
+          </div>
+          <div className={styles['right-half']}>
+            <img
+              src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+              alt="image 2"
+            />
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
+}
---
+++ components/Holder.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Holder() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div className='pt-6 p-4 shadow-sm shadow-gray-800 justify-center mx-auto relative flex flex-row flex-wrap flex-auto gap-x-44 sm:gap-y-6' style={{ display: 'inline-flex' }}>
+        <div>
+          <img src='http://www.cicba.in/cicba2023/images/springer-logo.png' alt="Springer Logo" />
+        </div>
+        <div className='flex flex-row pr-4'>
+          <img src="http://www.cicba.in/cicba2023/images/logo.png" alt="CICBA Logo" style={{ height: '100px', width: '101px' }} />
+          <img src="https://www.nitp.ac.in/static/logo512-579d034908ae89b5e0243d1274937d5d.png" alt="NITP Logo" style={{ height: '100px', width: '101px' }} />
+        </div>
+        <div>
+          <img src="http://www.cicba.in/cicba2023/images/ccis_logo.png" alt="CCIC Image" />
+        </div>
+      </div>
+</div>
+  );
+}
